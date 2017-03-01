d6cfc04e0eef797ffda040f06f967f6173ea6ab1
chaospredictor <kuzi81@gmail.com>
2017-03-01 16:39:12 +0200

    upload submenu added

--- main.py
+++ main.py
@@ -5,24 +5,37 @@ from additional import ConfigSectionMap
 Config = ConfigParser.ConfigParser()
 Config.read("./config/general.ini")
 
-def one():
+def main1():
 	print "connect to server\n"
 	os.system("ssh %s@%s -p %s"% (user, ip, port))
 
-def two():
-	print "\n"
-	os.system("scp -r -P %s %s/. %s@%s:~%s"% (port, fromFolder,user, ip, toFolder))
+def main2():
+	#print "\n"
+	uploadMenu()
 
-
-def three():
+def main3():
 	print "3\n"
 
-options = {
-  '1' : one,
-  '2' : two,
-  '3' : three,
+def folder1():
+	os.system("scp -r -P %s %s/. %s@%s:~%s"% (port, fromFolder,user, ip, toFolder))
+	
+def folder2():
+	return true
+
+def folder3():
+	return true
+
+mainOptions = {
+  '1' : main1,
+  '2' : main2,
+  '3' : main3,
 }
 
+uploadOptions = {
+	'1' : folder1,
+	'2' : folder2,
+	'3' : folder3,
+}
 
 user = ConfigSectionMap(Config, "Main")['user']
 ip = ConfigSectionMap(Config, "Main")['ip']
@@ -30,10 +43,18 @@ port = ConfigSectionMap(Config, "Main")['port']
 fromFolder = ConfigSectionMap(Config, "Folder")['from']
 toFolder = ConfigSectionMap(Config, "Folder")['to']
 
-c = raw_input("1: connect to server\n2: upload 01\n")
-os.system("clear")
+def main():
+	t = raw_input("1: connect to server\n2: upload\n")
+	#os.system("clear")
+	mainOptions[t]()
 
-options[c]()
 
+def uploadMenu():
+	print("\n")	
+	t = raw_input("1: folder1\n2: folder2\n")
+	os.system("clear")
+	uploadOptions[t]()	
+
+main()
 
 
